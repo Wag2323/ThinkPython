a00db313a39639ba5edc553bc0e90c99595f3898
Wag2323 <Wag2323@users.noreply.github.com>
2015-05-01 23:38:53 -0500

    added more functions

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def is_sorted(t):
     """takes a list of parameters and returns True if the list is sorted
@@ -14,20 +15,25 @@ def is_anagram(string1, string2):
     """Broken doesnt work with duplicate letters in a word 
     ex. hello and heloo : could fix maybe with dict and count number of 
     occurrences to see if they match for each word"""
-    
     if len(string1) != len(string2):
         return False
-        
+    
+    if has_duplicates(string1) and has_duplicates(string2):
+        return 
+    
     for letter in string1:
         if not(letter in string2):
             return False
         
     return True        
-
+    
 def has_duplicates(t):
     """check if any element appears more than once in a sequence"""
     return len(set(t)) < len(t)
     
+def remove_duplicates(t):
+    return list(set(t))
+   
 def rand_int_list(n, start, stop):
     """generates a random list of length n with numbers between  start and 
     stop including the start and stop"""
@@ -45,7 +51,25 @@ def count_matches(length, startrange, stoprange, samples):
         if has_duplicates(test):
             count += 1
     return count
-        
+    
+def make_word_list1():
+    """Reads lines from a file and builds a list using append."""
+    t = []
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        t.append(word)
+    return t
+
+def make_word_list2():
+    """Reads lines from a file and builds a list using list +."""
+    t = []
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        t = t + [word]
+    return t   
+    
     
 if __name__ == '__main__':
   
@@ -63,12 +87,33 @@ if __name__ == '__main__':
     # for n in range(0, 10):
         # print rand_int_list(5, 1, 31)
         
-    num_students = 23
-    rangestart = 1
-    rangestop = 365
-    num_simulations = 1000
-    count = count_matches(num_students, rangestart, rangestop, num_simulations)
+    # num_students = 23
+    # rangestart = 1
+    # rangestop = 365
+    # num_simulations = 1000
+    # count = count_matches(num_students, rangestart, rangestop, num_simulations)
     
-    print 'After %d simulations' % num_simulations
-    print 'with %d students' % num_students
-    print 'there were %d simulations with at least one match' % count
+    # print 'After %d simulations' % num_simulations
+    # print 'with %d students' % num_students
+    # print 'there were %d simulations with at least one match' % count
+    
+    # t = [1, 2, 4, 5, 5, 6, 5]
+    # print t
+    # s = remove_duplicates(t)
+    # print s
+    
+    start_time = time.time()
+    t = make_word_list1()
+    elapsed_time = time.time() - start_time
+    
+    print len(t)
+    print t[:10]
+    print elapsed_time, 'seconds'
+    
+    start_time = time.time()
+    t = make_word_list2()
+    elapsed_time = time.time() - start_time
+    
+    print len(t)
+    print t[:10]
+    print elapsed_time, 'seconds'
