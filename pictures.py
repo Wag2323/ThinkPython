672177ef7ed13e9ece058257887d6c82c7f6b7c9
R <Wag2323@users.noreply.github.com>
2015-05-05 15:57:22 -0500

    added functions
    Setup functions that need to be filled in

--- pictures.py
+++ pictures.py
@@ -1,7 +1,33 @@
+##############################################################################
+# This project will take a set of questions and ask the user to answer them. #
+# The correct answer will pull a picture out of the local directory of this  #
+# script and display it in the browser.                                      #
+############################################################################## 
+
 from PIL import Image
 import webbrowser
 
 # img = Image.open('engineer.jpg')
 # img.show()
 
-webbrowser.open('engineer.jpg')
+# webbrowser.open('engineer.jpg')
+
+def question_loop(questions):
+    """Takes a dictionary of question strings and prints them to the screen 
+    to be answered by the user"""
+    
+def word_list(answers):
+    """Prints the list of possible answers to the questions as a word bank"""
+    
+def remove_word(answer):
+    """Removes a word from the answer bank"""
+    
+def dict_to_list(questions):
+    """Strips out the answers from the question dictionary and puts them into 
+    a list"""
+    
+def open_picture(answer):
+    """Uses the answer to the question to open the picture that goes with it"""
+    
+    
+
